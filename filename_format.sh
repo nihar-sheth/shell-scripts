4999212fbc65c786075cba5abfeb3a17ea5f2a36
Nihar Sheth <niharsheth98@gmail.com>
2022-06-01 16:50:55 -0400

    Implemented underscore replace, cleaned up code and some visual touch-ups

--- filename_format.sh
+++ filename_format.sh
@@ -2,29 +2,31 @@
 # Date: 2022/05/31
 # Modified: 2022/06/01
 # Author: Nihar Sheth
-# Convert a file or set of files' names to a standard format I use: all lowercase with underscores as spaces.
-# Usage: $ ./filename_format.sh file ...
+# Convert a file or set of files' names to a standard format that I use across my personal directories.
+# Usage: $ ./filename_format.sh file [...]
 
 #!/usr/bin/env bash
 
-# Verify minimum number of arguments
+# Verify minimum number of arguments passed
 if [ $# -lt 1 ]; then
-	echo "ERROR: No files passed."
+	echo "❌ ERROR: No files passed."
 	exit 1
 fi
 
-# Verify all arguments passed are files
+# Verify all arguments passed are files before formatting
 for arg in "$@"; do
 	if ! [ -f "$arg" ]; then
-		echo "ERROR: $arg is not a file."
+		echo "❌ ERROR: $arg is not a file."
 		exit 1
 	fi
 done
 
+# Format each filename using absolute path for safety
 for file in "$@"; do
 	absolute_path=$(dirname "$(realpath "$file")")/
-	filename=$(basename "$file" | awk -F '/' '{print tolower($NF)}') # TODO: Implement underscore replace
-	mv "$file" "${absolute_path}$filename"
+	converted_filename=$(basename "$file" | awk '{print tolower($0)}' | tr " " "_")
+	mv "$file" "${absolute_path}${converted_filename}"
 done
 
+echo "✅ Filenames formatted!"
 exit 0
