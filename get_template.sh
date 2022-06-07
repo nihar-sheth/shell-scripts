52a8d73655957b9e3ab070d66f4c6aa8facd7d46
Nihar Sheth <niharsheth98@gmail.com>
2022-06-07 14:29:45 -0400

    Error handling in get_template.sh if header file does not exist

--- get_template.sh
+++ get_template.sh
@@ -2,11 +2,10 @@
 
 # get_template.sh
 # Date: 2022/06/06
-# Modified: 2022/06/06
+# Modified: 2022/06/07
 # Author: Nihar Sheth
 # Copy my standard script header template to the system clipboard.
 # Usage: $ get_template.sh
 
-readonly template_file="header_template.txt"
-[ -e "$template_file" ] && cat $template_file | pbcopy
-exit 0
+readonly template_file=".header_template.txt"
+[ -e "$template_file" ] && { cat $template_file | pbcopy; exit 0; } || { echo "❌ ERROR: No template file found."; exit 1; }
