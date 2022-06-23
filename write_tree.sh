517691bc93c24ae1a68f4b625ca257459cd60ab7
Nihar Sheth <niharsheth98@gmail.com>
2022-06-23 14:45:28 -0400

    write_tree.sh init

---
+++ write_tree.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# write_tree.sh
+# Date: 2022/06/23
+# Modified: 2022/06/23
+# Author: Nihar Sheth
+# Write a tree-formatted directory listing to a text file.
+# Usage: $ write_tree.sh [filename[.txt]]
+
+verify_arg() {
+    [[ "$1" =~ ^[A-z0-9_\-]+(\.txt)?$ ]] || { echo "❌ ERROR: Invalid filename." && exit 1; }
+}
+
+make_name() {
+    [[ "$1" =~ .\.txt ]] && echo "$1" || echo "$1.txt"
+}
+
+write_to_file() {
+    echo "Directory listing for $(pwd)" > "$1"
+    tree -dn >> "$1"
+}
+
+main() {
+    [[ -w "$(pwd)" ]] || { echo "⛔️ Current directory cannot be written to." && exit 1; }
+    case "$#" in
+        0) filename="directory_tree.txt" ;;
+        1) filename="$(make_name "$1")" ;;
+        *) echo "❌ ERROR: Too many arguments." && exit 1 ;;
+    esac
+
+    [[ -f "${filename}" ]] && {
+        read -p "Overwrite existing text file? [yn]: " selection
+        case "${selection}" in
+            y) : ;;
+            n) exit 0 ;;
+            *) echo "❌ ERROR: Invalid selection." && exit 1 ;;
+        esac
+    }
+    write_to_file "${filename}" 
+}
+
+main "$@" && exit 0
